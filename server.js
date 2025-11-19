e2eaffd4bb1888c5e2980a4feb7ffabc1ee9890f
Javier <javpascal@yahoo.es>
2025-11-19 13:44:50 +0100

    Configure for Vercel deployment

--- server.js
+++ server.js
@@ -91,6 +91,11 @@ app.post('/messages', async (req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
+
+if (process.env.NODE_ENV !== 'production') {
+    app.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}`);
+    });
+}
+
+export default app;
